84e2de5861807c845f7e456c06c76e685ae8c2c7
Poulami Saha <saha.poulami91@gmail.com>
2024-02-21 07:07:22 +0100

    Handling block error

--- Array/String/Remove-Element.ts
+++ Array/String/Remove-Element.ts
@@ -9,7 +9,7 @@
 // Input: nums = [3,2,2,3], val = 3
 // Output: 2, nums = [2,2,_,_]
 
-function removeElement(nums: number[], val: number): number {
+export function removeElement(nums: number[], val: number): number {
     let j=0;
     for(let i=0;i<nums.length;i++){
         if(nums[i]!==val){
